1d8f361b11e8d7806ac69ac94e799970df23e7d1
Marcin Mysliwiec <marcin.mysliw@gmail.com>
2021-10-20 21:34:52 +0200

    v1.0.1

--- UnixTimeStamp/UnixTimeStamp.cs
+++ UnixTimeStamp/UnixTimeStamp.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 
-public class UnixTimeStamp
+namespace UnixTimeStamp
 {
-    public static DateTime ToDateTime(int unixTimeStamp)
+    public static class Converter
     {
-        DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-        return dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-    }
+        public static DateTime ToDateTime(int unixTimeStamp)
+        {
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            return dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+        }
 
-    public static int FromDateTime(DateTime dateTime)
-    {
-        DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        TimeSpan diff = dateTime.ToUniversalTime() - origin;
-        return (int)diff.TotalSeconds;
+        public static int FromDateTime(DateTime dateTime)
+        {
+            var dtSeconds = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return (int)(dateTime.ToUniversalTime() - dtSeconds).TotalSeconds;
+        }
     }
 }
